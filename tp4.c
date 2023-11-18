152854b6e192df5cfcd77d3a1e096125d0d4acad
juannbasualdo <130847402+juannbasualdo@users.noreply.github.com>
2023-11-18 20:49:33 -0300

    hh

--- tp4.c
+++ tp4.c
@@ -5,7 +5,7 @@
 #define MAX_ROWS 2
 
 //A)
-int leerArchivo(char *nombreArchivo, float fuente[MAX_COLS], float canal[MAX_ROWS][MAX_COLS]) {
+int leerArchivo(char *nombreArchivo, float fuente[MAX_COLS], float matrizCanal[MAX_ROWS][MAX_COLS]) {
 
     FILE *archivo = fopen(nombreArchivo,"rt");
 
@@ -23,29 +23,58 @@ int leerArchivo(char *nombreArchivo, float fuente[MAX_COLS], float canal[MAX_ROW
         }
     }
 
-    //matriz del canal binario
+    //matriz del matrizCanal binario
     for ( int i = 0 ; i < MAX_ROWS ; i++ ) {
         for ( int j = 0 ; j < MAX_COLS ; j++) {
-            if (fscanf(archivo, "%f", &canal[i][j]) != 1) {
-                fprintf(stderr, "Error al leer la matriz del canal binario.\n");
+            if (fscanf(archivo, "%f", &matrizCanal[i][j]) != 1) {
+                fprintf(stderr, "Error al leer la matriz del matrizCanal binario.\n");
                 fclose(archivo);
                 exit(-1); //error
             }
         }
     }
 
+
+
     fclose(archivo);
     return 0;
 }
 
 
+//B)
+//Primero calculo las probabilidades a posteriori
+
+void calculaProbB(float probSalida[MAX_COLS]) { //calcula la probabilidad de que salga 0 y 1 sin saber que entro
+   
+   float suma = 0;
+
+   for ( int i = 0 ; i < 2 ; i++ ) {
+      for ( int j = 0 ; j < 2 ; j++ ) {
+        // suma += (fuente[j]*matrizCanal[i][j]);
+      }
+   }
+}
+
+void incisoB(float probPosteriori) {
+   for ( int i = 0 ; i < 4 ; i++ ) {
+      //post -> P(0\0)
+      //post -> P(0\1)
+      //post -> P(1\0)
+      //post -> P(1\1)
+
+
+   }
+}
+
 
 int main() {
 
-    float fuente[MAX_COLS];
-    float canal [MAX_ROWS][MAX_COLS];
+    float fuente[MAX_COLS]; //prob de que entre un 0 y de que entre un 1
+    float matrizCanal [MAX_ROWS][MAX_COLS];
+    float probPosteriori[MAX_ROWS][MAX_COLS];
+    float probSalida[MAX_COLS]; //probabilidad de que salga 0 y 1 sin saber que entro
 
-    (leerArchivo("tp4_sample4.txt", fuente, canal)); //A)
+    leerArchivo("tp4_sample4.txt", fuente, matrizCanal); //A)
        
 
     return 0;
